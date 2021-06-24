067a4c644352baf25e4b92eba2d300ec25e55bdd
titaniumbutter <israelfranco1998@gmail.com>
2021-06-24 14:57:59 -0500

    first commit

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+const express = require('express') 
+const app = express();
+
+const path = require('path')
+
+app.get('/', function(req, res) {
+
+    res.sendFile( path.join(__dirname, '../index.html'));
+
+})
+
+const port = process.env.PORT || 5060;
+
+app.listen (port, function() {
+    console.log(`Server is running out on ${port}`)
+})
