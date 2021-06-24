9732a0fe937a9733dfc25d3473df97ebe6ea2a7c
titaniumbutter <israelfranco1998@gmail.com>
2021-06-24 15:48:17 -0500

    8

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path')
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, '../styles.css'));
 
 app.get('/', function(req, res) {
 
